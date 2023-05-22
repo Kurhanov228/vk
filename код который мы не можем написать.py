976f704369c351b390f5779ba26eb257ac019654
txt <94191848+Kurhanov228@users.noreply.github.com>
2023-05-22 19:53:59 +0300

    glke

--- код который мы не можем написать.py
+++ код который мы не можем написать.py
@@ -1,11 +1,12 @@
 import requests
 from pprint import pprint
-access_token="vk1.a.4ICt4sW-F3dNIQivZndDp06NV1bLUKIydjDi5Td8159D_OmZ5OjBrknwBkMQbybzlhpxNDTGkvKYCbae9O6N-9mSv40BBwL78WKG-P0JaK3W497XHZeULdq4xwmNAu4D-MZ38sgJQI8iz92VsiAs_Ms6fHxMvRSmOBor25brtK_JYdyusSHs_-yJ72Y5_Sdr"
+access_token="vk1.a.nIHkcjq3jEomSr02e6YQ9j_sASzyLTeNgi-SyOh5skpxk1GKFXzgBmegjrKhqLf49-BVh4Vw1TC7AWMAwpOmoiK381ghUW03pZl8_bkdU9XqPIdFoFFf6h9_QQGk1NmcgHd0JLiwgYc7MNZ58Pj0Ehao5WnzdYDMHQka4jt88WeiVzFv4mXGMrKbfIGC7w6j"
 url = "https://api.vk.com/method/wall.get"
 params = {
     "access_token":access_token,
     "v":"5.81",
-    "owner_id":-204723494
+    "owner_id":-204723494,
+    "count":2
 }
 a = requests.get(url,params=params)
-pprint(a.json())
+pprint(a.json()["response"]["items"][0]["attachments"])
